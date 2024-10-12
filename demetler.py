68cd01fbe6633ee9963bab62f839efd3768d8b26
zorbeyyavas <yavaszorbey@gmail.com>
2024-10-12 16:21:13 +0300

    Add files via upload

---
+++ demetler.py
@@ -0,0 +1,15 @@
+# demetler listelere benzer yapıdadırla.fakat listeelr üzerinde oynama yapabilirken fakat demetler üzerinde tanımlama yapıldıktan sonra oynama yapılamaz
+demet1=("ali","veli",3,10)
+demet2=("ali","cem","can")
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#herhangi bir ifadenin veri tipini öğrenmek için type() kullanılır
+demet1=("ali","veli",3,10)
+print(type(demet1))
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#python dilinde demet ifadesinde 2 istisna vardır bunlar boş bir demet oluşturmak ve bir elemanlı demet oluşturma ifadesi
+demetbos=() #boş bir demet oluşturmak için kullanılır
+demettekeleman=("eleman",)  #tek elemanlı demet oluşturmak için kullanılır 
